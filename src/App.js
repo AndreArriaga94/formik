338cf17890052579946f830bc61daac88e1a2345
Ismar André Arriaga Lemus <arriaga109407@gmail.com>
2024-11-09 08:46:21 -0600

    se agrega el hook de useFormik y el archivo de Formik por componentes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import { useFormik } from "formik";
+import { Formik, Form, Field, ErrorMessage } from "formik";
+
+const validate = (values) => {
+  const errors = {}
+    if(!values.name){
+      errors.name = 'Requerido'
+    } else if(values.name.length < 5){
+      errors.name = 'El nombre es muy corto'
+    }
+    
+    if(!values.lastName){
+      errors.lastName = 'Requerido'
+    } else if(values.lastName.length < 5){
+      errors.lastName = 'El apellido es muy corto'
+    }
+  return errors
+}
 
 function App() {
-  const formik = useFormik({
-    initialValues:{
-      name:'',
-      lastName:'',
-      email:'',
-    },
-    onSubmit: values => console.log(values)
-  })
-  
   return (
-    <form onSubmit={formik.handleSubmit}>
-      <label>Nombre</label>
-      <input name="name" type="text" onChange={formik.handleChange} value={formik.values.name}/>
-      <label>Apellido</label>
-      <input name="lastName" type="text" onChange={formik.handleChange} value={formik.values.lastName}/>
-      <label>Email</label>
-      <input name="email" type="email" onChange={formik.handleChange} value={formik.values.email}/>
-      <button type="submit">Enviar</button>
-    </form>
+    <Formik
+        initialValues={{name:'',lastName:'',email:'',}}
+        validate={validate}
+        onSubmit={values => console.log(values)}
+        
+    >
+        
+        <Form>
+            <label>Nombre</label>
+            <Field name='name' type='text'/>
+            <ErrorMessage name='name'/>
+            <br></br>
+            <label>Apellido</label>
+            <Field name='lastName' type='text'/>
+            <ErrorMessage name='lastName'/>
+            <br></br>
+            <label>Email</label>
+            <Field name='email' type='email'/>
+            <ErrorMessage name='email'/>
+            <br></br>
+            <button type="submit">Enviar</button>
+        </Form>
+        
+    </Formik>
   );
 }
 
